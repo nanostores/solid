fd1c9b311e28eddc249fe23f7366bc104e85f0b7
Robert Soriano <sorianorobertc@gmail.com>
2021-08-13 23:32:24 +0800

    add test

---
+++ test/useStore.spec.tsx
@@ -0,0 +1,45 @@
+import { createStore } from 'nanostores'
+import { createSignal, Show } from 'solid-js'
+import { render } from 'solid-js/web'
+import { useStore } from '../src/useStore'
+
+it('renders simple store', async () => {
+    const events: string[] = []
+    
+    const letterStore = createStore<{ letter: string }>(() => {
+        events.push('constructor')
+        letterStore.set({ letter: 'a' })
+        return () => {
+            events.push('destroy')
+        }
+    })
+
+    const div = document.createElement('div')
+    const [show, setShow] = createSignal(true)
+
+    const dispose = render(() => {
+        const store = useStore(letterStore)
+
+        return (
+            <div>
+                { show() && <div data-testid="test1">{store.letter}</div> }
+            </div>
+        )
+    }, div)
+    
+    expect(events).toEqual(['constructor'])
+    expect(div.querySelector('[data-testid="test1"]').textContent).toBe('a')
+
+    letterStore.set({ letter: 'b' })
+    letterStore.set({ letter: 'c' })
+
+    expect(div.querySelector('[data-testid="test1"]').textContent).toBe('c')
+    setShow(false)
+    expect(div.querySelector('[data-testid="test1"]')).toBeNull()
+
+    dispose()
+    
+    setTimeout(() => {
+        expect(events).toEqual(['constructor', 'destroy'])
+    }, 0)
+})
