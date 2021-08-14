1044616a891223fe7528889cfb5c245b30f79e21
Robert Soriano <sorianorobertc@gmail.com>
2021-08-14 15:53:38 +0800

    fix initial value

--- src/solid-nanostores.ts
+++ src/solid-nanostores.ts
@@ -1,11 +1,9 @@
 import { createStore, reconcile } from 'solid-js/store';
 import { onCleanup } from 'solid-js';
-import { ReadableStore } from 'nanostores';
-import { ReadonlyIfObject } from 'nanostores/create-store';
+import { ReadableStore, getValue } from 'nanostores';
 
 export function useStore<T extends any>(store: ReadableStore<T>) {
-  // @ts-expect-error: Need to subscribe to get first value
-  const [state, setState] = createStore<ReadonlyIfObject<T> | undefined>();
+  const [state, setState] = createStore(getValue(store));
   const unsubscribe = store.subscribe((newState) => {
     setState(reconcile(newState))
   });
