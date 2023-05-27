e0f5ac27430dcf19b1f251fc3f1f1565196620ca
wobsoriano <sorianorobertc@gmail.com>
2023-05-26 19:55:04 -0700

    update file extension

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.tsx'],
   clean: true,
   format: ['cjs', 'esm'],
   dts: true,
